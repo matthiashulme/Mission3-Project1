63f801d9b83de98fc16e45cbffb246b523639511
Matthias Hulme <mh828@byu.edu>
2023-01-23 13:58:56 -0700

    In Class 01/23/2023

--- Mission 3 - Project 1/Program.cs
+++ Mission 3 - Project 1/Program.cs
@@ -6,7 +6,61 @@ namespace Mission_3___Project_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Welcome
+            Console.WriteLine("Welcome to Tik Tak Toe Buddy :) !");
+
+            // Create Game Board with stored default numbers and Initialize Player and Done Booleans
+            string[] gameBoard = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+            bool player1 = true;
+            bool done = false;
+
+            Support s = new Support();
+
+            // While Loop for each turn until the game finishes
+            while (done == false)
+            {
+                // Print Game Board
+                s.boardPrint(gameBoard);
+
+                int pick = 0;
+                // If Player 1
+                if (player1 == true)
+                {
+                    // Take Guess and Update Game Board
+                    Console.Write("Player 1 pick a slot: ");
+                    pick = Convert.ToInt32(Console.Read());
+                    gameBoard[pick - 1] = "X";
+
+                    player1 = false;
+                }
+                // If Player 2
+                else
+                {
+                    // Take Guess and Update Game Board
+                    Console.Write("Player 2 pick a slot: ");
+                    pick = Convert.ToInt32(Console.Read());
+                    gameBoard[pick - 1] = "O";
+
+                    player1 = true;
+                }
+                // Call boardWinner Method in Support Class
+                bool[] result = s.boardWinner(gameBoard);
+                
+                // Display results if Win
+                if (result[0] == true)
+                {
+                    done = true;
+                    if (result[1] == true)
+                    {
+                        Console.WriteLine("Player 1 Wins!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 2 Wins!");
+                    }
+                }
+            }
         }
     }
 }
---
+++ Mission 3 - Project 1/Support.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Mission_3___Project_1
+{
+    public class Support
+    {
+        int[] array = new array[] {1,2,3,4,5,6,7,8,9};
+
+        public string boardPrint(int[] array)
+        {
+            print(array[0] + "|" + array[1] + "|" + array[2] + "\n" +
+            "-|-|-\n" +
+            array[3] + "|" + array[4] + "|" + array[5] + "\n" +
+            "-|-|-\n" +
+            array[6] + "|" + array[7] + "|" + array[8]);
+        }    
+        
+
+        public string boardWinner(int[] array)
+        {
+            
+        }
+    }
+}
