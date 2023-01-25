a3f8f36c5e82a18d835e974835718add134e9f30
Matthias Hulme <mh828@byu.edu>
2023-01-25 10:25:39 -0700

    Final Commit

--- Mission 3 - Project 1/Program.cs
+++ Mission 3 - Project 1/Program.cs
@@ -7,7 +7,7 @@ namespace Mission_3___Project_1
         static void Main(string[] args)
         {
             // Welcome
-            Console.WriteLine("Welcome to Tik Tak Toe Buddy :) !");
+            Console.WriteLine("Welcome to Tik Tak Toe Buddy :) !\n");
 
             // Create Game Board with stored default numbers and Initialize Player and Done Booleans
             string[] gameBoard = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -21,16 +21,53 @@ namespace Mission_3___Project_1
             while (done == false)
             {
                 // Print Game Board
+                Console.Write("\n");
                 s.boardPrint(gameBoard);
 
-                int pick = 0;
+                string pick = "";
                 // If Player 1
                 if (player1 == true)
                 {
                     // Take Guess and Update Game Board
-                    Console.Write("Player 1 pick a slot: ");
-                    pick = Convert.ToInt32(Console.Read());
-                    gameBoard[pick - 1] = "X";
+                    Console.Write("\nPlayer 1 pick a slot: ");
+                    pick = Console.ReadLine();
+
+                    if (pick == "1")
+                    {
+                        gameBoard[0] = "X";
+                    }
+                    else if (pick == "2")
+                    {
+                        gameBoard[1] = "X";
+                    }
+                    else if (pick == "3")
+                    {
+                        gameBoard[2] = "X";
+                    }
+                    else if (pick == "4")
+                    {
+                        gameBoard[3] = "X";
+                    }
+                    else if (pick == "5")
+                    {
+                        gameBoard[4] = "X";
+                    }
+                    else if (pick == "6")
+                    {
+                        gameBoard[5] = "X";
+                    }
+                    else if (pick == "7")
+                    {
+                        gameBoard[6] = "X";
+                    }
+                    else if (pick == "8")
+                    {
+                        gameBoard[7] = "X";
+                    }
+                    else if (pick == "9")
+                    {
+                        gameBoard[8] = "X";
+                    }
 
                     player1 = false;
                 }
@@ -38,9 +75,45 @@ namespace Mission_3___Project_1
                 else
                 {
                     // Take Guess and Update Game Board
-                    Console.Write("Player 2 pick a slot: ");
-                    pick = Convert.ToInt32(Console.Read());
-                    gameBoard[pick - 1] = "O";
+                    Console.Write("\nPlayer 2 pick a slot: ");
+                    pick = Console.ReadLine();
+
+                    if (pick == "1")
+                    {
+                        gameBoard[0] = "O";
+                    }
+                    else if (pick == "2")
+                    {
+                        gameBoard[1] = "O";
+                    }
+                    else if (pick == "3")
+                    {
+                        gameBoard[2] = "O";
+                    }
+                    else if (pick == "4")
+                    {
+                        gameBoard[3] = "O";
+                    }
+                    else if (pick == "5")
+                    {
+                        gameBoard[4] = "O";
+                    }
+                    else if (pick == "6")
+                    {
+                        gameBoard[5] = "O";
+                    }
+                    else if (pick == "7")
+                    {
+                        gameBoard[6] = "O";
+                    }
+                    else if (pick == "8")
+                    {
+                        gameBoard[7] = "O";
+                    }
+                    else if (pick == "9")
+                    {
+                        gameBoard[8] = "O";
+                    }
 
                     player1 = true;
                 }
@@ -50,17 +123,23 @@ namespace Mission_3___Project_1
                 // Display results if Win or Draw
                 if (result == 1)
                 {
-                    Console.WriteLine("Player 1 Wins!");
+                    Console.Write("\n");
+                    s.boardPrint(gameBoard);
+                    Console.WriteLine("\nPlayer 1 Wins!");
                     done = true;
                 }
                 else if (result == 2)
                 {
-                    Console.WriteLine("Player 2 Wins!");
+                    Console.Write("\n");
+                    s.boardPrint(gameBoard);
+                    Console.WriteLine("\nPlayer 2 Wins!");
                     done = true;
                 }
                 else if (result == 3)
                 {
-                    Console.WriteLine("Cat's Game!");
+                    Console.Write("\n");
+                    s.boardPrint(gameBoard);
+                    Console.WriteLine("\nCat's Game!");
                     done = true;
                 }
             }
--- Mission 3 - Project 1/Support.cs
+++ Mission 3 - Project 1/Support.cs
@@ -2,23 +2,71 @@ using System;
 
 namespace Mission_3___Project_1
 {
-    public class Support
+    class Support
     {
-        int[] array = new array[] {1,2,3,4,5,6,7,8,9};
-
-        public string boardPrint(int[] array)
+        //Board Print method. Takes the updated board from the program and prints it out in the normal tik tack toe shape
+        public void boardPrint(string[] array)
         {
-            print(array[0] + "|" + array[1] + "|" + array[2] + "\n" +
-            "-|-|-\n" +
-            array[3] + "|" + array[4] + "|" + array[5] + "\n" +
-            "-|-|-\n" +
-            array[6] + "|" + array[7] + "|" + array[8]);
-        }    
+        Console.WriteLine(array[0] + "|" + array[1] + "|" + array[2] + "\n" +
+        "-|-|-\n" +
+        array[3] + "|" + array[4] + "|" + array[5] + "\n" +
+        "-|-|-\n" +
+        array[6] + "|" + array[7] + "|" + array[8]);
+        }
+        // Decision making method, where it looks at the board and determines what to do next
+        public int boardWinner(string[] array)
+        {
+        // winningCombinations is pretty much every way in which a player can win
+        string[] winningCombinations =
+        {
+            array[0] + array[1] + array[2],
+            array[3] + array[4] + array[5],
+            array[6] + array[7] + array[8],
+            array[0] + array[3] + array[6],
+            array[1] + array[4] + array[7],
+            array[2] + array[5] + array[8],
+            array[0] + array[4] + array[8],
+            array[2] + array[4] + array[6]
+        };
+        // this loop is to find if a player has one of the winning combinations
+        for (int i = 0; i < winningCombinations.Length; i++)
+        {
+            // if x wins then give a 1 for player 1 winner
+            if (winningCombinations[i].Equals("XXX"))
+            {
+            return 1;
+            }
+            // if o wins then give a 2 for player 2 to win
+            else if (winningCombinations[i].Equals("OOO"))
+            {
+            return 2;
+            }
+        }
+        if (array[0].Equals("1")
+            ||
+            array[1].Equals("2")
+            ||
+            array[2].Equals("3")
+            ||
+            array[3].Equals("4")
+            ||
+            array[4].Equals("5")
+            ||
+            array[5].Equals("6")
+            ||
+            array[6].Equals("7")
+            ||
+            array[7].Equals("8")
+            ||
+            array[8].Equals("9"))
+            {
+                return 0;
+            }
         
-
-        public string boardWinner(int[] array)
-        {
-            
+            else
+            {
+                return 3;
+            }
         }
     }
 }
